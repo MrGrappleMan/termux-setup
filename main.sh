3be05aadd72cce4453085ec03aeb2ad4b36572f4
MrGrappleMan <90239357+MrGrappleMan@users.noreply.github.com>
2026-03-03 13:44:29 +0530

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,16 +1,24 @@
+# Basic Setup
+
 termux-setup-package-manager
 termux-setup-storage
+
+apt update
 pkg update
+apt full-upgrade -y
 pkg upgrade -y
 # Essentials + Languages
 pkg install -y wget curl aria2 proot proot-distro root-repo x11-repo git python hollywood fastfetch tar
+pkg autoclean
 
-# Kali NetHunter ( [1] full image )
-wget -O install-nethunter-termux https://offs.ec/2MceZWr
-chmod +x install-nethunter-termux
-./install-nethunter-termux
+# Image installation
 
-# Ubuntu 22.04 Andronix
+## Kali NetHunter ( [1] full image )
+#wget -O install-nethunter-termux https://offs.ec/2MceZWr
+#chmod +x install-nethunter-termux
+#./install-nethunter-termux
+
+## Ubuntu 22.04 lxQt - from Andronix
 wget https://raw.githubusercontent.com/AndronixApp/AndronixOrigin/master/Installer/Ubuntu22/ubuntu22-lxqt.sh -O ubuntu22-lxqt.sh
 chmod +x ubuntu22-lxqt.sh
 bash ubuntu22-lxqt.sh
