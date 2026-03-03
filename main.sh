c82578b83d1c256164d7b273d3e8f0905cce0894
MrGrappleMan <90239357+MrGrappleMan@users.noreply.github.com>
2026-03-03 22:39:44 +0530

    Refine view

--- main.sh
+++ main.sh
@@ -1,24 +1,33 @@
+#-This is the main script from which you start execution of
+#-the rest of the script
+
 # Basic Setup
+## Enviroment permissions and setup
 
 termux-setup-package-manager
 termux-setup-storage
+
+## Package Management
+#-We firstly use apt to get hands on control and resolve dependancies
+#-Then pkg used for standard usage
 
 apt update
 pkg update
 apt full-upgrade -y
 pkg upgrade -y
-# Essentials + Languages
+
+### Package install
 pkg install -y wget curl aria2 proot proot-distro root-repo x11-repo git python hollywood fastfetch tar
+
 pkg autoclean
 
-# Image installation
+# Distro selection screen
 
-## Kali NetHunter ( [1] full image )
-#wget -O install-nethunter-termux https://offs.ec/2MceZWr
-#chmod +x install-nethunter-termux
-#./install-nethunter-termux
+## Kali NetHunter
+wget -O getDistro.sh https://offs.ec/2MceZWr
 
-## Ubuntu 22.04 lxQt - from Andronix
-wget https://raw.githubusercontent.com/AndronixApp/AndronixOrigin/master/Installer/Ubuntu22/ubuntu22-lxqt.sh -O ubuntu22-lxqt.sh
-chmod +x ubuntu22-lxqt.sh
-bash ubuntu22-lxqt.sh
+## Ubuntu 22.04 LXQt - from Andronix
+wget -O getDistro.sh https://raw.githubusercontent.com/AndronixApp/AndronixOrigin/master/Installer/Ubuntu22/ubuntu22-lxqt.sh
+
+chmod +x getDistro.sh
+bash getDistro.sh
